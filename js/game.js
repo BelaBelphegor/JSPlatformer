b816a4ff5cd444692cf3f1905dd071325caac9cc
Timothee <tiboitel@student.42.fr>
2017-02-16 00:11:47 +0100

    Adding jump when key UP_ARROW is pressed.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 var plateform_game = function() {
 	this.c = new Coquette(this, "plateform-js-canvas", 500, 500, "#000");
-	this.c.entities.create(player, { center: {x:32, y:484}, color:"#f07"});
+	this.c.gravity = 0.2;
+	this.c.friction = 0.8;
+	this.c.entities.create(player, { center: {x:32, y:494}, color:"#f07"});
 };
 
 var player = function(game, settings) {
@@ -11,6 +13,7 @@ var player = function(game, settings) {
 	this.speed = 3;
 	this.velocity_x = 0;
 	this.velocity_y = 0;
+	this.jumping = false;
 	this.draw = function(ctx)
 	{
 		ctx.fillStyle = settings.color;
@@ -22,18 +25,36 @@ var player = function(game, settings) {
 		{
 			if (this.velocity_x > -this.speed)
 				this.velocity_x--;
-			this.center.x += this.velocity_x;
 		}
 		if (this.c.inputter.isDown(this.c.inputter.RIGHT_ARROW))
 		{
 			if (this.velocity_x < this.speed)
 				this.velocity_x++;
-			this.center.x += this.velocity_x;
 		}
+		if (this.c.inputter.isDown(this.c.inputter.UP_ARROW))
+		{
+			if (!this.jumping)
+			{
+				this.jumping = true;
+				this.velocity_y = -this.speed * 2;
+			}
+		}
+		this.velocity_x *= this.c.friction;
+		this.velocity_y += this.c.gravity;
+
+		this.center.y += this.velocity_y;
+		this.center.x += this.velocity_x;
 		if (this.center.x >= 500 - this.size.x / 2)
 			this.center.x = 500 - this.size.x / 2;
 		if (this.center.x <= 0 + this.size.x / 2)
 			this.center.x = 0 + this.size.x / 2;
+		if (this.center.y >= 500  - this.size.y / 2)
+		{
+			this.center.y = 500 - this.size.y / 2;
+			this.jumping = false;
+		}
+		console.log(this.center.y);
+		console.log(this.jumping);
 	};
 };
 
